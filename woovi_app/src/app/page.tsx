0ae14b94ebb56748a82914eec96ed15e3698c337
Darlan Carvalho <92957575+DarlanSantosCarvalho@users.noreply.github.com>
2024-07-11 09:49:34 -0300

    Feat: Adição de checkbutton

--- woovi_app/src/app/page.tsx
+++ woovi_app/src/app/page.tsx
@@ -1,3 +1,8 @@
+import * as React from "react";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
 import Image from "next/image";
 import Logo from "../assets/Logo.png";
 
@@ -10,6 +15,51 @@ export default function Home() {
           <h2 className="font-bold text-xl">João, como você quer pagar?</h2>
         </div>
       </main>
+      <FormControl className="flex items-center">
+        <RadioGroup
+          className="w-[100%]"
+          aria-labelledby="demo-radio-buttons-group-label"
+          defaultValue="female"
+          name="radio-buttons-group"
+        >
+          <div className="border border-6 border-gray-300 w-[90%] mx-auto my-8 rounded-xl p-4 relative">
+            <span className="font-bold border border-6 px-4 py-2 bg-gray-300 rounded-[100%] border-gray-300 absolute top-[-25px]">
+              Pix
+            </span>
+            <FormControlLabel
+            className="font-bold text-black"
+              value="female"
+              control={<Radio />}
+              label="1x R$ 30.500,00"
+              labelPlacement="start"
+            />
+          </div>
+          <div className="border border-6 border-gray-300 w-[90%] mx-auto my-5 rounded-xl p-4">
+            <FormControlLabel
+              value="female"
+              control={<Radio />}
+              label="Female"
+              labelPlacement="start"
+            />
+          </div>
+          <div className="border border-6 border-gray-300 w-[90%] mx-auto my-5 rounded-xl p-4">
+            <FormControlLabel
+              value="female"
+              control={<Radio />}
+              label="Female"
+              labelPlacement="start"
+            />
+          </div>
+          <div className="border border-6 border-gray-300 w-[90%] mx-auto my-5 rounded-xl p-4">
+            <FormControlLabel
+              value="female"
+              control={<Radio />}
+              label="Female"
+              labelPlacement="start"
+            />
+          </div>
+        </RadioGroup>
+      </FormControl>
     </body>
   );
 }
