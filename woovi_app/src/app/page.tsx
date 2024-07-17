c82703dc9917710adbcd26d7cf4c5c16364387f6
Darlan Carvalho <darlan171201@gmail.com>
2024-07-17 15:23:03 -0300

    Feat: Adição de novas features

--- woovi_app/src/app/page.tsx
+++ woovi_app/src/app/page.tsx
@@ -4,8 +4,17 @@ import Logo from "../assets/Logo.png";
 import Image from "next/image";
 import { FaEye } from "react-icons/fa";
 import { FaEyeSlash } from "react-icons/fa";
+import { useRouter } from "next/router";
 
 export default function mainPage() {
+  const router = useRouter();
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  function changePasswordToText() {
+    setShowPassword(!showPassword);
+  }
+
   return (
     <body>
       <main className="flex flex-col justify-between">
@@ -19,25 +28,44 @@ export default function mainPage() {
         <h1 className="text-3xl text-center mt-14 font-bold text-white">
           Seja bem-vindo ao Sunshine Bank
         </h1>
-        <div className="flex flex-col justify-center items-center relative">
-          <input
-            type="text"
-            placeholder="Email ou nome de usuário"
-            className="py-4 mt-10 rounded-lg w-[18%] placeholder: text-center"
-          />
-          <input
-            type="password"
-            placeholder="Senha"
-            className="py-4 mt-10 rounded-lg w-[18%] placeholder: text-center"
-          />
-          <FaEye size={30} className="absolute bottom-[118px] right-[800px]" />
+        <div>
+          <div className="flex flex-col justify-center items-center">
+            <input
+              type="text"
+              placeholder="Email ou nome de usuário"
+              className="p-4 py-4 mt-10 rounded-lg w-[60%] placeholder: text-center md:w-[40%] lg:w-[18%]"
+            />
+            <div className="relative w-[60%] md:w-[40%] lg:w-[18%] mt-10">
+              <input
+                type={showPassword ? "text" : "password"}
+                placeholder="Senha"
+                className="p-4 py-4 rounded-lg w-full placeholder:text-center"
+              />
+              {showPassword ? (
+                <FaEyeSlash
+                  onClick={changePasswordToText}
+                  size={30}
+                  className="cursor-pointer text-black absolute right-3 top-1/2 transform -translate-y-1/2"
+                />
+              ) : (
+                <FaEye
+                  onClick={changePasswordToText}
+                  size={30}
+                  className="cursor-pointer text-black absolute right-3 top-1/2 transform -translate-y-1/2"
+                />
+              )}
+            </div>
 
-          <button className="w-[10%] py-4 rounded-xl mt-10 text-2xl font-bold bg-white hover:bg-orange-400 hover:text-white hover:duration-300">
-            Login
-          </button>
+            <button
+              onClick={() => router.push("/metodoPagamento")}
+              className="py-4 rounded-xl mt-10 text-2xl font-bold w-[40%] placeholder: text-center md:w-[40%] lg:w-[10%] bg-white hover:bg-orange-400 hover:text-white hover:duration-300"
+            >
+              Login
+            </button>
+          </div>{" "}
         </div>
       </main>
-      <html className="bg-amarelo"></html>
+      <html className="bg-laranja"></html>
     </body>
   );
 }
--- woovi_app/tailwind.config.ts
+++ woovi_app/tailwind.config.ts
@@ -9,7 +9,7 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        amarelo: "#f4672c",
+        laranja: "#f4672c",
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
