22a0cd52f9bc962da296271196a8909011acaa0b
Darlan Carvalho <darlan171201@gmail.com>
2024-07-24 10:24:37 -0300

    Feat: Validação concluída

--- woovi_app/src/app/page.tsx
+++ woovi_app/src/app/page.tsx
@@ -22,12 +22,12 @@ export default function MainPage() {
   const router = useRouter();
   const [showPassword, setShowPassword] = useState(false);
 
-  function logIn() {
-    router.push("/metodoPagamento");
-  }
-
   function changePasswordToText() {
     setShowPassword(!showPassword);
+  }
+
+  function logIn() {
+    router.push("/metodoPagamento");
   }
 
   return (
@@ -47,25 +47,34 @@ export default function MainPage() {
           <div>
             <div className="flex flex-col justify-center items-center">
               <input
-                {...(register("username"), { required: true })}
+                {...(register("username"),
+                {
+                  required: true,
+                  minLength: 7,
+                  maxLength: 40,
+                })}
                 type="text"
-                placeholder="Email ou nome de usuário"
+                placeholder="Email de acesso"
                 className="p-4 py-4 mt-10 rounded-lg w-[60%] placeholder: text-center md:w-[40%] lg:w-[18%]"
               />
               {errors.username && (
-                <span className="font-bold text-red-600">
+                <span className="font-bold text-white">
                   Esse campo é obrigatório
                 </span>
               )}
               <div className="relative w-[60%] md:w-[40%] lg:w-[18%] mt-10">
                 <input
-                  {...register("password", { required: true })}
+                  {...(register("password"),
+                  {
+                    required: true,
+                    minLength: 7,
+                  })}
                   type={showPassword ? "text" : "password"}
                   placeholder="Senha"
                   className="p-4 py-4 rounded-lg w-full placeholder:text-center"
                 />
                 {errors.password && (
-                  <span className="font-bold text-red-600">
+                  <span className="font-bold text-white">
                     Esse campo é obrigatório
                   </span>
                 )}
